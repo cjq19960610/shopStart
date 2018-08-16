5948c0166de49681d54355d7fd575b9a5c74baa2
叫我老岑就好啊 <42258396+cjq19960610@users.noreply.github.com>
2018-08-16 10:34:13 +0800

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,38 @@
+package menu;
+
+public class Data {
+
+    int[] customNo = new int[200];
+    String[] customBirth = new String[200];
+    double[] customScore = new double[200];
+
+    public void init() {
+        customNo[0] = 1111;
+        customBirth[0] = "10/02";
+        customScore[0] = 1000;
+
+        customNo[1] = 2222;
+        customBirth[1] = "12/02";
+        customScore[1] = 3000;
+
+        customNo[2] = 3333;
+        customBirth[2] = "12/02";
+        customScore[2] = 4000;
+
+        customNo[3] = 2488;
+        customBirth[3] = "08/22";
+        customScore[3] = 4000;
+
+        customNo[4] = 1534;
+        customBirth[4] = "01/02";
+        customScore[4] = 3300;
+
+        customNo[5] = 2654;
+        customBirth[5] = "09/02";
+        customScore[5] = 2560;
+
+        customNo[6] = 1001;
+        customBirth[6] = "10/02";
+        customScore[6] = 1000;
+    }
+}
